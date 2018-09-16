550c3593f97edd96e2c57e8da662068373cd7aed
BrOrlandi <brorlandi@gmail.com>
2018-09-16 03:49:44 -0300

    Add comment form.

--- src/js/CommentList.js
+++ src/js/CommentList.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Comment } from 'semantic-ui-react';
+import { Comment, Form } from 'semantic-ui-react';
 
 import CommentItem from './CommentItem';
 
 class CommentList extends Component {
+  onSubmitComment = (e) => {
+    console.log(e.target.comment.value);
+  }
+
   render() {
-    const { comments } = this.props;
+    const { comments } = this.props; // TODO icon
     return (
       <Comment.Group>
         {comments.map(comment => (<CommentItem {...comment} />))}
+        <Form onSubmit={this.onSubmitComment}>
+          <Form.Group>
+            <Form.Input placeholder="Comment..." name="comment" />
+            <Form.Button content="Comentar" />
+          </Form.Group>
+        </Form>
       </Comment.Group>
     );
   }
