dc37b1a7a699423bab9d8e9668882b2634bb5950
BrOrlandi <brorlandi@gmail.com>
2018-09-16 03:18:24 -0300

    Firebase config.

---
+++ src/js/config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'AIzaSyDsbhCZzdaH039531UkAJp3iD8I1Sg93A0',
+  authDomain: 'instafire-92e8f.firebaseapp.com',
+  databaseURL: 'https://instafire-92e8f.firebaseio.com',
+  projectId: 'instafire-92e8f',
+  storageBucket: '',
+  messagingSenderId: '399346260685',
+};
+
+export default config;
