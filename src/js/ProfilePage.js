32903673dc9fb5bb47f1e0299f99134e06efaeb0
BrOrlandi <brorlandi@gmail.com>
2018-09-16 03:18:10 -0300

    WIP ProfilePage

---
+++ src/js/ProfilePage.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+
+class ProfilePage extends Component {
+  render() {
+    return (
+      <div>
+        profile
+      </div>
+    );
+  }
+}
+
+export default ProfilePage;
