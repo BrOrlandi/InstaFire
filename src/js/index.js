0ba6580cfd0992ca5001a8927a1f7f034effb4e9
BrOrlandi <brorlandi@gmail.com>
2018-09-16 03:50:02 -0300

    Temp auth firebase.

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,22 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import '../styles/index.styl';
 
+// import { auth } from './rebase';
+
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
+// const renderApp = (user) => {
+//   ReactDOM.render(<App user={user} />, document.getElementById('app'));
+// };
+
+// const handleAuthStateChanged = (user) => {
+//   if (user) {
+//     console.log('Authenticated with', user.displayName);
+//   }
+
+//   renderApp(user);
+// };
+
+// auth.onAuthStateChanged(handleAuthStateChanged);
