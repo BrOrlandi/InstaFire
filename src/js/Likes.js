fdd2220a290992a93b4fef3d6b086a83d97b1029
BrOrlandi <brorlandi@gmail.com>
2018-09-17 00:57:13 -0300

    Like button working.

---
+++ src/js/Likes.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Icon } from 'semantic-ui-react';
+import firebase, { rebase } from './firebase';
+
+class Likes extends Component {
+  state = {
+    likes: [],
+  };
+
+  componentDidMount() {
+    const { photoKey } = this.props;
+    rebase.syncState(`/photos/${photoKey}/likes`, {
+      defaultValue: [],
+      context: this,
+      asArray: true,
+      state: 'likes',
+    });
+  }
+
+  onClickHeart = () => {
+    const { likes } = this.state;
+    const { photoKey } = this.props;
+    const userId = firebase.auth().currentUser.uid;
+
+    const index = likes.indexOf(userId);
+    const liked = index > -1;
+
+    if (!liked) {
+      rebase.push(`/photos/${photoKey}/likes`, { data: userId });
+      return;
+    }
+
+    const unliked = likes.filter(like => like !== userId);
+
+    this.setState({
+      likes: unliked,
+    });
+  };
+
+  render() {
+    const { likes } = this.state;
+    const userId = firebase.auth().currentUser.uid;
+    const liked = likes.indexOf(userId) > -1;
+
+    return (
+      <div className="photo__likes">
+        {!liked && <Icon name="heart outline" size="big" onClick={this.onClickHeart} />}
+        {liked && <Icon name="heart" size="big" color="red" onClick={this.onClickHeart} />}
+        {likes.length}
+      </div>
+    );
+  }
+}
+
+Likes.propTypes = {
+  photoKey: PropTypes.string,
+};
+
+export default Likes;
--- src/js/Photo.js
+++ src/js/Photo.js
@@ -3,63 +3,48 @@ import PropTypes from 'prop-types';
 import {
   Header,
   Image,
-  Icon,
   Segment,
 } from 'semantic-ui-react';
 
-import CommentList from './CommentList';
+// import CommentList from './CommentList';
+import Likes from './Likes';
 
-class Photo extends Component {
-  static propTypes = {
-    userPicture: PropTypes.string,
-    userName: PropTypes.string,
-    imageSrc: PropTypes.string,
-    description: PropTypes.string,
-    timestamp: PropTypes.number,
-    photoKey: PropTypes.string,
-  };
+const Photo = ({
+  photoKey,
+  userPicture,
+  userName,
+  imageSrc,
+  description,
+  timestamp,
+}) => {
+  const dateString = (new Date(timestamp)).toLocaleString();
 
-  onClickHeart = () => {
-    console.log('heart click');
-  };
+  return (
+    <Segment className="photo">
+      <Header as="h4" className="photo__header">
+        <Image src={userPicture} avatar />
+        <span className="photo__header">
+          <span>{userName}</span>
+        </span>
+        <span className="photo__date">{dateString}</span>
+      </Header>
+      <Image src={imageSrc} fluid />
+      <div className="photo__footer">
+        <Likes photoKey={photoKey} />
+        <h3 className="photo__description">{description}</h3>
+        {/* <CommentList photoKey={photoKey} /> */}
+      </div>
+    </Segment>
+  );
+};
 
-  render() {
-    const {
-      photoKey,
-      userPicture,
-      userName,
-      imageSrc,
-      description,
-      timestamp,
-    } = this.props;
-
-    const likes = [];
-    const liked = likes.indexOf('123') > -1;
-
-    const dateString = (new Date(timestamp)).toLocaleString();
-
-    return (
-      <Segment className="photo">
-        <Header as="h4" className="photo__header">
-          <Image src={userPicture} avatar />
-          <span className="photo__header">
-            <span>{userName}</span>
-          </span>
-          <span className="photo__date">{dateString}</span>
-        </Header>
-        <Image src={imageSrc} fluid />
-        <div className="photo__footer">
-          <div className="photo__likes">
-            {!liked && <Icon name="heart outline" size="big" onClick={this.onClickHeart} />}
-            {liked && <Icon name="heart" size="big" color="red" onClick={this.onClickHeart} />}
-            {likes.length}
-          </div>
-          <h3 className="photo__description">{description}</h3>
-          {/* <CommentList photoKey={photoKey} /> */}
-        </div>
-      </Segment>
-    );
-  }
-}
+Photo.propTypes = {
+  userPicture: PropTypes.string,
+  userName: PropTypes.string,
+  imageSrc: PropTypes.string,
+  description: PropTypes.string,
+  timestamp: PropTypes.number,
+  photoKey: PropTypes.string,
+};
 
 export default Photo;
