b53c0111561e0314a99f4e5b40314ef8307662bd
BrOrlandi <brorlandi@gmail.com>
2018-09-17 02:03:03 -0300

    CommentList WIP.

--- src/js/CommentList.js
+++ src/js/CommentList.js
@@ -2,9 +2,35 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Comment, Form } from 'semantic-ui-react';
 
+import { rebase } from './firebase';
 import CommentItem from './CommentItem';
 
+const comments = [
+  {
+    key: 'c3',
+    userId: '123',
+    userPhoto: 'https://picsum.photos/48/?image=453',
+    userName: 'João da Silva',
+    message: 'Comentário teste 3',
+  },
+  {
+    key: 'c4',
+    userId: '1234',
+    userPhoto: 'https://picsum.photos/48/?image=449',
+    userName: 'Maria da Silva',
+    message: 'Comentário teste 4',
+  },
+];
+
 class CommentList extends Component {
+  state = {
+    comments: [],
+  };
+
+  componentDidMount() {
+    // rebase.bindToState('co');
+  }
+
   onSubmitComment = (e) => {
     const commentText = e.target.comment.value;
     if (!commentText) {
@@ -15,7 +41,7 @@ class CommentList extends Component {
   }
 
   render() {
-    const { comments } = this.props;
+    // const { comments } = this.state;
     return (
       <Comment.Group>
         {comments.map(comment => (<CommentItem {...comment} />))}
